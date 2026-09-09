8499dea08e025a7080f0fa568fdbf36798054742
nayanacs485-lab <chandanacs485@gmail.com>
2026-09-09 23:55:18 +0530

    Add Valid Anagram solution

---
+++ Arrays-Strings/ValidAnagram.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
